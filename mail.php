4c7ce446101a0e17135d39711506b35aebe7eb60
Oskolsky Maxim <info@oskolsky.com>
2014-04-01 12:20:58 +0400

    .

--- mail.php
+++ mail.php
@@ -11,7 +11,7 @@
     $mail -> To($to);
     $mail -> Cc($cc);
     $mail -> Subject('Обратная связь с сайта Театральный дом');
-    $mail -> Body('Имя: '.$data['name']."\n".'Телефон: '.$data['phone']."\n".'Удобное время: '.$data['time']);
+    $mail -> Body('Имя: '.$data['name']."\n".'Телефон: '.$data['phone']."\n".'Удобное время: '.$data['period']);
     $mail -> Priority(3);
     $mail -> Send();
     echo $mail -> Get();
